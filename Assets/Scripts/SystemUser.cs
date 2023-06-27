e13ed28f49a327f0ffea72ea40498f8ed91a6a26
fabiolealsc <fabio.leal.sc@gmail.com>
2023-06-27 16:22:08 -0300

    Update SystemUser.cs

--- Assets/Scripts/SystemUser.cs
+++ Assets/Scripts/SystemUser.cs
@@ -11,6 +11,7 @@ public class SystemUser : MonoBehaviour
     public Recorder recorder;
     public TextMeshPro nome;
     public PhotonView photonView;
+
     public string playerName;
     public static bool change;
     void Start()
@@ -57,21 +58,5 @@ public class SystemUser : MonoBehaviour
     }
     void Update()
     {
-        if (photonView.IsMine)
-        {
-            float rotation = Input.GetAxis("Horizontal");
-            float speed = Input.GetAxis("Vertical");
-
-            Quaternion rot = rb.rotation * Quaternion.Euler(0, rotation * Time.deltaTime * 60, 0);
-            rb.MoveRotation(rot);
-            Vector3 force = rot * Vector3.forward * speed * 1000 * Time.deltaTime;
-
-            rb.AddForce(force);
-
-            if (rb.velocity.magnitude > 2)
-            {
-                rb.velocity = rb.velocity.normalized * 2;
-            }
-        }
     }
 }
